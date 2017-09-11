f630083ce6afc526b7f507594fae0188488cd32c
lam tran <lb.tran648@gmail.com>
2017-09-10 18:00:06 -0700

    add style on homepage

--- src/components/homepage/home-page-body.js
+++ src/components/homepage/home-page-body.js
@@ -1,10 +1,56 @@
-import React from 'react';
-import carpic from '../../images/carpic.jpg';
+import React,{Component} from 'react';
+import {scroller} from 'react-scroll';
 
-export default function HomePageBody(){
-  return(
-    <div>
-      <img src={carpic} className="img-fluid" alt="Responsive image"/>
-    </div>
-  )
+
+import carpic from '../../images/carpic.jpg';
+import xehoi from '../../images/xehoi.jpeg';
+
+
+class HomePageBody extends Component{
+  
+  handleScroll(){
+    scroller.scrollTo('sect2',{
+      duration: 1500,
+      delay: 100,
+      smooth: "easeInOutQuint"
+    })
+  }
+  
+  render(){
+    var bgImg={
+      backgroundImage: `url(${carpic})`
+    }
+    var bgImg2 = {
+      backgroundImage: `url(${xehoi})`
+    }
+    return(
+      <div>
+        <div className="container-fluid">
+          <section className="row">
+            <div className="home" style={bgImg}>
+              <h1>This is Danh Pham Startup Car Dealer</h1>
+              <div className="down-btn">
+                <a className="btn" onClick={() => this.handleScroll()}>
+                  <i className="fa fa-arrow-circle-down fa-4x" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+          </section>
+          <section className="row">
+            <div className="content"></div>
+          </section>
+          <section id="sect2" className="row">
+            <div className="home fixed-attachment" style={bgImg2}>
+              hello2
+            </div>
+          </section>
+          <div className="row">
+            <div className="content"></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
+
+export default HomePageBody;
