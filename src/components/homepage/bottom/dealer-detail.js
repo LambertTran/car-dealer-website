9530d696d734c0477fb3b7b0f5e431147c567a84
lam tran <lb.tran648@gmail.com>
2017-09-24 17:29:17 -0700

    re-style homepage

--- src/components/homepage/bottom/dealer-detail.js
+++ src/components/homepage/bottom/dealer-detail.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import Footer from './footer';
 
 /** Images **/
 import stickshift from '../../../images/stickshift3.jpg';
+import Footer from './footer';
 
 export default class DealerDetail extends Component{
   render(){
@@ -12,30 +12,35 @@ export default class DealerDetail extends Component{
     return(
       <section className="row">
         <div className="home fixed-attachment reduced-size" style={bgImg2}>
-          <div className="row">
-            <h1>Auto 24</h1>
-            <h1>(916) 993-0999</h1>
-            <div className="col-12 col-sm-12 col-lg-8 dealer-detail">
-              <ul>
-                <li> We are CARFAX Advantage® Dealer </li>
-                <li> FREE CARFAX® Vehicle History Reports™ on every car</li>
-                <li> Buy from us with confidence </li>
-              </ul>
-            </div>
-            <div className="col-12 col-sm-12 col-lg-4 time">
-              <ul>
-                <li>Mon: 10:00AM - 6:30PM</li>
-                <li>Tue:  10:00AM - 6:30PM</li>
-                <li>Wed:  10:00AM - 6:30PM</li>
-                <li>Thu:  10:00AM - 6:30PM</li>
-                <li>Fri:  10:00AM - 6:30PM</li>
-                <li>Sat:  10:00AM - 6:30PM</li>
-                <li>Sun:  10:00AM - 6:30PM</li>
-              </ul>
+          <div className="col-12">
+            <div className="row">
+              <h1>Auto 24</h1>
+              <h1>(916) 993-0999</h1>
+              <div className="col-12 col-sm-12 col-lg-8 dealer-detail">
+                <ul>
+                  <li> We are CARFAX Advantage® Dealer </li>
+                  <li> FREE CARFAX® Vehicle History Reports™ on every car</li>
+                  <li> Buy from us with confidence </li>
+                </ul>
+              </div>
+              <div className="col-12 col-sm-12 col-lg-4 time">
+                <ul>
+                  <li>Mon: 10:00AM - 6:30PM</li>
+                  <li>Tue:  10:00AM - 6:30PM</li>
+                  <li>Wed:  10:00AM - 6:30PM</li>
+                  <li>Thu:  10:00AM - 6:30PM</li>
+                  <li>Fri:  10:00AM - 6:30PM</li>
+                  <li>Sat:  10:00AM - 6:30PM</li>
+                  <li>Sun:  10:00AM - 6:30PM</li>
+                </ul>
             </div>
           </div>
-          <Footer />
+          </div>
+            <Footer />
         </div>
+
+            
+
       </section>
     )
   }
